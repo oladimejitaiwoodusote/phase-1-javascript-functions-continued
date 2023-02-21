209f74eea7c033bd9ca9fda8324758640f82b9e9
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-02-21 14:29:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(String = "*"){
+    return function (Parameter = "special"){
+        return `You are ${String}${Parameter}${String}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("%")("a dedicated programmer");
+console.log(encouragingPromptFunction);
